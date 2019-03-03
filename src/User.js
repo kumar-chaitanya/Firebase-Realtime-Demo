c0d88e259a0e6652e3e42cb8415ebc3a817ff366
kumar-chaitanya <16103244chaitanya@gmail.com>
2019-03-03 21:26:22 +0530

    Removed dependency on local storage

--- src/User.js
+++ src/User.js
@@ -3,39 +3,12 @@ import firebase from './config';
 import ids from './join_id';
 import { isArray } from 'util';
 
-const addToStorage = key => {
-  let pending =  JSON.parse(localStorage.getItem('pending'));
-  
-  if(!pending) {
-    pending = [];
-  }
-
-  pending.push(key);
-  localStorage.setItem('pending', JSON.stringify(pending));
-}
-
-const getFromStorage = () => {
-   let pending = JSON.parse(localStorage.getItem('pending'));
-   if(!pending)
-      return [];
-    else
-      return pending;
-}
-
-const removeFromStorage = key => {
-  let pending = JSON.parse(localStorage.getItem('pending'));
-  let index = pending.indexOf(key);
-  let t = pending.splice(index, 1);
-  localStorage.setItem('pending', JSON.stringify(pending));
-  return index;
-}
-
 const updateStatus = (key, that) => {
   let {userId} = ids;
-  let index = removeFromStorage(key);
   let pending = [...that.state.pending];
+  let index = pending.findIndex(item => item.messageId === key);
   let reference = pending.splice(index, 1);
-  firebase.database().ref(`user/${userId}/messages/${key}`).off('child_changed', reference[0]);
+  firebase.database().ref(`user/${userId}/messages/${key}`).off('child_changed', reference[0].fn);
   let messages = [...that.state.messages];
   index = messages.findIndex(message => key === message.messageId);
   let message = messages.splice(index, 1);
@@ -55,11 +28,10 @@ export class User extends Component {
   }
 
   componentWillUnmount() {
-    let items = getFromStorage();
     let {userId} = ids;
     let pending = [...this.state.pending];
-    items.forEach((item, i) => {
-      firebase.database().ref(`user/${userId}/messages/${item}`).off('child_changed', pending[i]);
+    pending.forEach(item => {
+      firebase.database().ref(`user/${userId}/messages/${item.messageId}`).off('child_changed', item.fn);
     });
     this.setState({pending: []});
   }
@@ -69,6 +41,7 @@ export class User extends Component {
     const rootRef = firebase.database().ref();
     const userRef = rootRef.child('user').child(userId);
     const messageRef = userRef.child('messages');
+    const query = messageRef.orderByChild('status').equalTo('Sent');
 
     messageRef.once('value', snapshot => {
       if(snapshot.val()){
@@ -81,15 +54,21 @@ export class User extends Component {
       }
     });
 
-    let pending = getFromStorage();
-    pending.forEach(item => {
-      let messageRef = firebase.database().ref(`user/${userId}/messages/${item}`);
-      let that = this;
-      let fn = messageRef.on('child_changed', snapshot => {
-        if(snapshot.val() === 'Seen')
-          updateStatus(item, that);
+    query.once('value', snapshot => {
+      snapshot.forEach(snap => {
+        let that = this;
+        let {messageId} = snap.val();
+        let mRef = firebase.database().ref(`user/${userId}/messages/${messageId}`);
+        let fn = mRef.on('child_changed', snapshot => {
+          if(snapshot.val() === 'Seen')
+            updateStatus(messageId, that);
+        });
+        let handleRef = {
+          messageId,
+          fn
+        };
+        this.setState({pending: this.state.pending.concat(handleRef)});
       });
-      this.setState({pending: this.state.pending.concat(fn)});
     });
   }
 
@@ -112,7 +91,6 @@ export class User extends Component {
       [`user/${userId}/messages/${key}`]: data,
       [`shop/${shopId}/messages/${key}`]: data
     }).then(() => {
-      addToStorage(key);
       let messageRef = firebase.database().ref(`user/${userId}/messages/${key}`);
       let that = this;
       let fn = messageRef.on('child_changed', snapshot => {
@@ -121,7 +99,11 @@ export class User extends Component {
           updateStatus(key, that);
       });
       let messages = [...this.state.messages, data];
-      this.setState({messages: [...messages], text: '', pending: this.state.pending.concat(fn)});
+      let handleRef = {
+        messageId: key,
+        fn
+      };
+      this.setState({messages: [...messages], text: '', pending: this.state.pending.concat(handleRef)});
     }).catch(error => console.log(error));
   }
 
